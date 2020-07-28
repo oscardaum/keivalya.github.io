174286d16780ba0ffd0b172c05c473c4f2fcdddd
Keivalya Pandya <61532731+keivalya@users.noreply.github.com>
2020-07-28 11:52:47 +0530

    Add files via upload

--- js/demo.js
+++ js/demo.js
@@ -6,8 +6,8 @@
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#FF3361',
+    lineColor: '#7433FF'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
